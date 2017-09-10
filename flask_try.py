3c7a3cee25fd53e114c99a12f201346e83676c57
AEliu <95649311@qq.com>
2017-09-10 16:26:43 +0800

    Initial commit

--- flask_try.py
+++ flask_try.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -8,13 +9,21 @@ app = Flask(__name__)
 def index():
     return 'Hello World! <br>This is the index page.'
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello World!'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_world(name=None):
+    return render_template('hello.html', name=name)
 
 @app.route('/user/<username>')
 def show_user(username):
     return 'Hi, %s' % username
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return render_template('hello.html', name=request.form['username'])
+    else:
+        return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
